b3f44823c79bb9174ef35e34b48ff23a00f14402
Grzegorz Jędrzejowski <g.jedrzejows@student.uw.edu.pl>
2026-03-19 22:04:47 +0100

    start project WUM2

--- Lab/generateEvents.sh
+++ Lab/generateEvents.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 #This scripts assumes you are inside the container. Run /home/gjedrzej/startGenieCustom.sh
 #Positional arguments should be events, process name, and the desired cross section relative path
-# Example: ./generateEvents.sh 1000000 "nu_tau" "/scratch1/gjedrzej/Lab/splines_3_6_02_Pythia8/splines_3_06_02_nuTau_Ar40_80_P8.xml"
+# Example: ./generateEvents.sh 100000 nu_tau /scratch1/gjedrzej/Lab/splines_3_6_02_Pythia8/splines_3_06_02_nuTau_Ar40_80_P8.xml
 
 Events=$1
 Process=$2
@@ -18,7 +19,7 @@ echo "Using splines: $Sigma"
 mkdir -p data
 
 gevgen -n $Events \
-       -e 3.5,50.0 \
+       -e 0.5,80 \
        -p 16 \
        --cross-sections $Sigma \
        -t 1000180400 \
