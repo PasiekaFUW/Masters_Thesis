b90d75f1a75ee8d7c717bbd8282d2afd7c902dec
Grzegorz Jędrzejowski <g.jedrzejows@student.uw.edu.pl>
2026-03-24 18:18:56 +0100

    ZmianaAnalizyDanych

---
+++ Lab/Generators/generateEventsNC2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#This scripts assumes you are inside the container. Run /home/gjedrzej/startGenieCustom.sh
+#Positional arguments should be events, process name, and the desired cross section relative path
+# Example: ./generateEvents.sh 100000 nu_tau /scratch1/gjedrzej/Lab/splines_3_6_02_Pythia8/splines_3_06_02_nuTau_Ar40_80_P8.xml
+
+Events=200000
+Process=nu_tau_NC2
+Sigma=/scratch1/gjedrzej/Lab/splines_3_6_02_Pythia8/splines_3_06_02_nuTau_Ar40_80_P8.xml
+
+# if [ -z "$3" ]; then
+#     echo "Usage: ./generateEvents.sh <events> <process_name> <spline_path>"
+#     exit 1
+# fi
+
+echo "Starting generation of $Events events for $Process..."
+echo "Using splines: $Sigma"
+
+mkdir -p data
+
+gevgen -n $Events \
+       -e 3,10 \
+       -p 16 \
+       --cross-sections $Sigma \
+       -t 1000180400 \
+       --event-generator-list NC \
+       --message-thresholds $GENIE/config/Messenger_laconic.xml \
+       --mc-job-status-refresh-rate 500 \
+       --seed 12345 \
+       -o "data/$Process"_interactions.root
+
+echo "Output saved as data/${Process}_interactions.root"
--- Lab/convertEvents.sh
+++ Lab/convertEvents.sh
@@ -2,4 +2,8 @@
 
 File="$1"
 #File="/scratch1/gjedrzej/Lab/data/nu_tau_3_5__50_interactions.root"
-/app/Generator-R-3_06_02_Pythia8/bin/gntpc -i data/$File -f gst
+# /app/Generator-R-3_06_02_Pythia8/bin/gntpc -i $File -f gst
+
+
+
+/app/Generator-R-3_06_02_Pythia8/bin/gntpc -i $File -f rootracker
